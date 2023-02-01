d24ec73160bda8bbd82a02ec61abe62681ffc2af
Everson Henrich da Silva <eversonhs.es@gmail.com>
2023-02-01 17:52:20 -0300

    Adicionado ponte H com motores

--- embarcados_robo.ino
+++ embarcados_robo.ino
@@ -1,18 +1,7 @@
-#include <Stepper.h> 
-
-const int stepsPerRevolution = 64; 
-
-#define VELOCIDADE 300
-
-#define M1_IN1 8
-#define M1_IN2 9
-#define M1_IN3 10
-#define M1_IN4 11
-
-#define M2_IN1 42
-#define M2_IN2 43
-#define M2_IN3 44
-#define M2_IN4 45
+#define M1_IN1 11
+#define M1_IN2 10
+#define M2_IN1 9
+#define M2_IN2 8
 
 #define BT_ESQ 34
 #define BT_FRT 35
@@ -20,10 +9,7 @@ const int stepsPerRevolution = 64;
 #define BT_DIR 37 
 
 #define P_ECHO 2
-#define P_TGR 3
-
-//Stepper motor1(stepsPerRevolution, M1_IN1, M1_IN3, M1_IN2, M1_IN4);
-Stepper motor2(stepsPerRevolution, M2_IN1, M2_IN3, M2_IN2, M2_IN4);
+#define P_TRG 3
 
 void setup() {
   pinMode(BT_ESQ, INPUT);
@@ -32,54 +18,91 @@ void setup() {
   pinMode(BT_DIR, INPUT);
 
   pinMode(P_ECHO, INPUT);
-  pinMode(P_TGR, OUTPUT);
+  pinMode(P_TRG, OUTPUT);
 
-//  motor1.setSpeed(VELOCIDADE);
-  motor2.setSpeed(VELOCIDADE);
+  pinMode(M1_IN1, OUTPUT);
+  pinMode(M1_IN2, OUTPUT);
+  
+  pinMode(M2_IN1, OUTPUT);
+  pinMode(M2_IN2, OUTPUT);
+  
   Serial.begin(9600);
 }
 
-float calcular_distancia(int p_trg, int p_ech) {
-  digitalWrite(p_trg, LOW);
+float calcular_distancia() {
+  digitalWrite(P_TRG, LOW);
   delayMicroseconds(2);
-  digitalWrite(p_trg, HIGH);
+  digitalWrite(P_TRG, HIGH);
   delayMicroseconds(2);
-  digitalWrite(p_trg, LOW);
+  digitalWrite(P_TRG, LOW);
   delayMicroseconds(2);
 
-  unsigned long tempo = pulseIn(p_ech, HIGH);
+  unsigned long tempo = pulseIn(P_ECHO, HIGH);
   float distancia = tempo / 29 /2; 
   return distancia;        
 }
 
 void moverFrente() {
-  //motor1.step(500);
-   //for (int i = 10; i<=60; i=i+10){
-   //motor1.setSpeed(i);
-   //motor1.step(40*i);
-   //}
-  for (int i = 0; i<=3; i++)
-   {
-   motor2.step(-512); 
-   delay(2000);
-   }
+  digitalWrite(M1_IN1, HIGH);
+  digitalWrite(M1_IN2, LOW);
+
+  digitalWrite(M2_IN1, HIGH);
+  digitalWrite(M2_IN2, LOW);
 }
 
+void moverTras() {
+  digitalWrite(M1_IN1, LOW);
+  digitalWrite(M1_IN2, HIGH);
+
+  digitalWrite(M2_IN1, LOW);
+  digitalWrite(M2_IN2, HIGH);
+}
+
+void moverDir() {
+  digitalWrite(M1_IN1, LOW);
+  digitalWrite(M1_IN2, HIGH);
+
+  digitalWrite(M2_IN1, HIGH);
+  digitalWrite(M2_IN2, LOW);
+}
+
+void moverEsq() {
+  digitalWrite(M2_IN1, LOW);
+  digitalWrite(M2_IN2, HIGH);
+
+  digitalWrite(M1_IN1, HIGH);
+  digitalWrite(M1_IN2, LOW);
+}
+
+void parar() {
+  digitalWrite(M1_IN1, LOW);
+  digitalWrite(M1_IN2, LOW);
+
+  digitalWrite(M2_IN1, LOW);
+  digitalWrite(M2_IN2, LOW);
+}
+
+bool ligado = false;
 
 void loop() {
-    Serial.print("besq: ");
-    Serial.println(digitalRead(BT_ESQ));
-    
-    Serial.print("bfrt: ");
-    Serial.println(digitalRead(BT_FRT));
-    
-    Serial.print("bonoff: ");
-    Serial.println(digitalRead(BT_ONOFF));
-
-    Serial.print("bDIR: ");
-    Serial.println(digitalRead(BT_DIR));
-    if(digitalRead(BT_DIR) == LOW){
-      moverFrente();
-    }
+    ligado = digitalRead(BT_ONOFF) == LOW ? !ligado : ligado;
     delay(100);
+    parar();
+    if(ligado) {
+      if(digitalRead(BT_DIR) == LOW){
+        moverDir();
+      }
+      if(digitalRead(BT_ESQ) == LOW){
+        moverEsq();
+      }
+      if(digitalRead(BT_FRT) == LOW){
+        moverFrente();
+      }
+      if(calcular_distancia() < 15.0) {
+        moverTras();
+        delay(1000);
+        moverDir();
+        delay(1000);
+      }    
+    }
 }
